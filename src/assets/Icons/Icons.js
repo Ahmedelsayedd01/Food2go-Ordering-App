304b7cf7b416dbcfa1fd1a69c15e4ab0c5bfda5b
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-18 15:39:15 +0200

    Add Icon

---
+++ src/assets/Icons/Icons.js
@@ -0,0 +1,3 @@
+export { default as EditIcon } from "./AnotherIcons/EditIcon";
+export { default as DeleteIcon } from "./AnotherIcons/DeleteIcon";
+export { default as WarningIcon } from "./AnotherIcons/WarningIcon";
