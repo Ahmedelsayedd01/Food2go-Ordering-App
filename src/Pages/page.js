e0953901f14e9f954ba2bb265a0fa65b5359acc2
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-18 15:41:21 +0200

    add Page

---
+++ src/Pages/page.js
@@ -0,0 +1,19 @@
+/* Authentication */
+// Login
+export { default as LoginPage } from "./Authentication/Login/LoginPage";
+// SignUP
+export { default as SignUpPage } from "./Authentication/SignUp/SignUpPage";
+
+/* Home */
+export { default as HomePage } from "./Home/HomePage";
+/* Menu */
+export { default as MenuPage } from "./Menu/MenuPage";
+/* ContactUs */
+export { default as ContactUsPage } from "./ContactUs/ContactusPage";
+/* Cart */
+export { default as CartPage } from "./Cart/CartPage";
+/* CheckOut */
+export { default as CheckOutPage } from "./CheckOut/CheckOutPage";
+
+/* Not Found Page */
+export { default as NotFoundPage } from "./NotFound/NotFoundPage";
