4c206d65b0f79fa3709a82a9e1e9d54fc447eaf0
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-18 15:40:57 +0200

    Add Custom Component

---
+++ src/Components/Components.js
@@ -0,0 +1,22 @@
+/* Buttons */
+export { default as StaticButton } from "./Buttons/StaticButton";
+export { default as SubmitButton } from "./Buttons/SubmitButton";
+export { default as AddButton } from "./Buttons/AddButton";
+/* Inputs */
+export { default as TextInput } from "./Inputs/TextInput";
+export { default as NumberInput } from "./Inputs/NumberInput";
+export { default as EmailInput } from "./Inputs/EmailInput";
+export { default as PasswordInput } from "./Inputs/PasswordInput";
+export { default as DateInput } from "./Inputs/DateInput";
+export { default as TimeInput } from "./Inputs/TimeInput";
+export { default as UploadInput } from "./Inputs/UploadInput";
+/* Spinners */
+export { default as CustomSpinner } from "./Spinners/CustomSpinner";
+export { default as StaticSpinner } from "./Spinners/StaticSpinner";
+
+/* Anthore Components */
+export { default as SearchBar } from "./AnotherComponents/SearchBar";
+export { default as TitlePage } from "./AnotherComponents/TitlePage";
+export { default as TitleSection } from "./AnotherComponents/TitleSection";
+export { default as Switch } from "./AnotherComponents/Switch";
+export { default as DropDown } from "./AnotherComponents/DropDown";
