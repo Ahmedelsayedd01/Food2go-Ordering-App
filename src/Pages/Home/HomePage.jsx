fb0cb54acb674cc9c54ff51406a6b6fd0a4cbbf7
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-12-19 14:08:00 +0200

    add Home page

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import HeaderSection from './Sections/HeaderSection'
+import ReviewSection from './Sections/ReviewSection'
+import BestDealsSection from './Sections/BestDealsSection'
+import BannerSection from './Sections/BannerSection'
+import PremiumSection from './Sections/PremiumSection'
 
 const HomePage = () => {
        return (
-              <div>HomePage</div>
+              <>
+                     <div className="w-full">
+                            <HeaderSection />
+                            <div className="w-full px-12">
+                                   <PremiumSection />
+                                   <BannerSection />
+                                   <BestDealsSection />
+                                   <ReviewSection />
+                            </div>
+                     </div>
+              </>
        )
 }
 
