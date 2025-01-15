7921d35894c89405843bd80b775c959a6cc7165f
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-01-15 10:14:42 +0200

    set data from api

--- src/Pages/Menu/Sections/CategoriesNavSection.jsx
+++ src/Pages/Menu/Sections/CategoriesNavSection.jsx
@@ -1,86 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts, setProductsFilter } from '../../../Store/CreateSlices';
 
 const CategoriesNavSection = () => {
-       const [activeTab, setActiveTab] = useState('All');
+       const dispatch = useDispatch();
 
-       const handleClick = (tabName) => {
+       const categories = useSelector(state => state.categories?.data);
+       const products = useSelector(state => state.products?.data);
+
+       const [activeTab, setActiveTab] = useState('All');
+       const [categoriesFilter, setCategoriesFilter] = useState([]);
+
+
+       console.log('categories', categories)
+       console.log('categories Filter', categoriesFilter)
+
+       useEffect(() => {
+              setCategoriesFilter(categories)
+       }, [categories])
+
+       const handleClick = (tabName, id) => {
               setActiveTab(tabName);
+              filterProduct(id);
        };
+
+       const filterProduct = (categoryId) => {
+              if (!categoryId) {
+
+                     dispatch(setProductsFilter(products))
+              } else {
+
+                     const newProduct = products.filter((product) => categoryId === product.category_id);
+                     dispatch(setProductsFilter(newProduct))
+                     console.log('newProduct', newProduct)
+              }
+              console.log('categoryId', categoryId)
+       }
        return (
               <>
-                     <div className="w-full flex flex-wrap items-center justify-start gap-5">
+                     <div className="w-full flex flex-wrap items-center justify-start gap-3">
                             <div
-                                   onClick={() => handleClick('All')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'All' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
+                                   onClick={() => handleClick('All', '')}
+                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'All' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-1 rounded-full`}>
                                    <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
+                                          className='w-10 h-10 rounded-full'
                                           alt="category"
+                                          loading='lazy'
                                    />
-                                   <span className='text-2xl'>All</span>
+                                   <span className='text-xl'>All</span>
                             </div>
-                            <div
-                                   onClick={() => handleClick('Burger')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Burger' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Burger</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Pastries')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Pastries' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Pastries</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Candies')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Candies' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Candies</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Pasta')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Pasta' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Pasta</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Pizza')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Pizza' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Pizza</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Meat')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Meat' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Meat</span>
-                            </div>
-                            <div
-                                   onClick={() => handleClick('Chickens')}
-                                   className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === 'Chickens' ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-2 rounded-full`}>
-                                   <img src="/src/assets/Images/IconNavFilter.png"
-                                          className='w-10 h-10'
-                                          alt="category"
-                                   />
-                                   <span className='text-2xl'>Chickens</span>
-                            </div>
+                            {categoriesFilter.map((category, index) => (
+                                   <div
+                                          key={index}
+                                          onClick={() => handleClick(category?.name, category?.id)}
+                                          className={`w-auto border-2 border-mainColor transition-all ease-in-out duration-300 cursor-pointer ${activeTab === category?.name ? 'bg-mainColor text-white' : 'bg-white text-mainColor'} hover:bg-mainColor hover:text-white  flex justify-between items-center gap-4 px-3 py-1 rounded-full`}>
+                                          <img src={category.image_link}
+                                                 className='w-10 h-10 rounded-full'
+                                                 alt="category"
+                                          />
+                                          <span className='text-xl'>{category?.name || '-'}</span>
+                                   </div>
+                            ))}
 
 
                      </div>
--- src/Pages/Menu/Sections/ItemsMenuSection.jsx
+++ src/Pages/Menu/Sections/ItemsMenuSection.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { CardItem } from '../../../Components/Components'
+import { useSelector } from 'react-redux'
 
 const ItemsMenuSection = () => {
+       const productsFilter = useSelector(state => state.productsFilter?.data);
+
        return (
               <>
-                     <div className="flex flex-wrap items-center justify-between gap-3 w-full sm:flex-col sm:gap-5 lg:flex-row mb-8">
-                            {Array(8).fill(null).map((_, index) => (
-                                   <CardItem key={index} />
+                     <div className="flex flex-wrap items-center justify-center gap-3 w-full sm:flex-col sm:gap-5 lg:flex-row mb-8">
+                            {productsFilter.map((product, index) => (
+                                   <CardItem key={index} product={product} />
                             ))}
 
                      </div>
