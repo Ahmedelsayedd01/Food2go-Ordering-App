a6d79386a0b0330a948e6d3cd47308a211e83166
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-01-22 23:22:12 +0200

    New Update

---
+++ src/Pages/CheckOut/Sections/DetailsOrder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DetailsOrder = () => {
+       return (
+              <>
+                     <div className="w-full border-mainColor border-[3px] rounded-2xl p-3">
+                            DetailsOrder
+                     </div>
+              </>
+       )
+}
+
+export default DetailsOrder
